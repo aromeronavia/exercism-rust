a115eb0580788c7d9cc658c425074735d7ae588f
Alberto Romero <aromeronavia@gmail.com>
2021-09-30 01:51:18 -0500

    Add rpg exercise

---
+++ role-playing-game/src/lib.rs
@@ -0,0 +1,40 @@
+// This stub file contains items which aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+pub struct Player {
+    pub health: u32,
+    pub mana: Option<u32>,
+    pub level: u32,
+}
+
+impl Player {
+    pub fn revive(&self) -> Option<Player> {
+        if self.health > 0 {
+            return None
+        } else {
+            Some(Player {
+                health: 100,
+                mana: if self.level >= 10 { Some(100) } else { None },
+                level: self.level
+            })
+        }
+    }
+
+    pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
+        match self.mana {
+            None => {
+                self.health = if self.health < mana_cost { 0 } else { self.health - mana_cost };
+                0
+            },
+            Some(current_mana) => {
+                if current_mana < mana_cost {
+                    0
+                } else {
+                    self.mana = Some(current_mana - mana_cost);
+                    mana_cost * 2
+                }
+            }
+        }
+    }
+}
