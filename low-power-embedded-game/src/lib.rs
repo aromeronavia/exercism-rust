461bf2b2dc157830eec5cbdc6686a61cf6833ca6
Alberto Romero <aromeronavia@gmail.com>
2021-09-30 01:22:35 -0500

    Add low power embedded game exercise

---
+++ low-power-embedded-game/src/lib.rs
@@ -0,0 +1,16 @@
+#![allow(unused)]
+
+pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
+    (dividend / divisor, (dividend % divisor))
+}
+
+pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
+    iter.step_by(2)
+}
+
+pub struct Position(pub i16, pub i16);
+impl Position {
+    pub fn manhattan(&self) -> i16 {
+        i16::abs(self.0) + i16::abs(self.1)
+    }
+}
