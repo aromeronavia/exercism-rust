1b1a6b4397057afdc3e6f677ad291754b7d04aae
Alberto Romero <aromeronavia@gmail.com>
2021-10-01 01:00:34 -0500

    get rid of mut variables for dicts

--- magazine-cutout/src/lib.rs
+++ magazine-cutout/src/lib.rs
@@ -12,12 +12,10 @@ pub fn build_map(mut acc: HashMap<&'static str, i8>, word: &&'static str) -> Has
     }
     acc
 }
-pub fn can_construct_note(magazine: &[&'static str], note: &[&'static str]) -> bool {
-    let mut magazine_map: HashMap<&str, i8> = HashMap::new();
-    let magazine_words = magazine.iter().fold(magazine_map, build_map);
 
-    let mut note_map: HashMap<&str, i8> = HashMap::new();
-    let note_words = note.iter().fold(note_map, build_map);
+pub fn can_construct_note(magazine: &[&'static str], note: &[&'static str]) -> bool {
+    let magazine_words = magazine.iter().fold(HashMap::new(), build_map);
+    let note_words = note.iter().fold(HashMap::new(), build_map);
 
     note_words.iter().fold(true, |acc, (word, value)| {
         if magazine_words.get(word) < Some(value) {
