f9dd65ccc0cc859a5a3566bdcb9f8506c765b05c
Alberto Romero <aromeronavia@gmail.com>
2021-10-05 00:41:04 -0500

    Finish luhn

---
+++ luhn/src/lib.rs
@@ -0,0 +1,38 @@
+/// Check a Luhn checksum.
+pub fn is_valid(code: &str) -> bool {
+    println!("INPUT {}", code);
+
+    let number = code
+        .chars()
+        .filter(|x| !x.is_whitespace())
+        .map(|x| x as i32 - 0x30)
+        .collect::<Vec<i32>>();
+
+    if number.len() <= 1 {
+        return false;
+    }
+
+    if number.iter().any(|x| *x < 0 || *x > 9) {
+        return false;
+    }
+
+    let sum = number.iter().rev().enumerate().fold(0, |acc, (index, x)| {
+        println!("acc: {}", acc);
+        if index % 2 == 1 {
+            let mut product = x * 2;
+            if product > 9 {
+                product -= 9;
+            }
+
+            println!("X: {}, product: {}", x, product);
+            acc + product
+        } else {
+            println!("X: {}", x);
+            acc + x
+        }
+    });
+
+    println!("Sum: {}", sum);
+
+    sum % 10 == 0
+}
